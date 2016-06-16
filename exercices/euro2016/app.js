10e705c499f21758dca8bc45bd2e989cb0bdc9ff
Formation <formation@MacBook-Pro-de-David-2.local>
2016-06-16 09:51:59 +0200

    First commit

---
+++ exercices/euro2016/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('myApp', []);
+
+app.factory('appFactory', ['$http', function($http){
+    return{
+        recupData: function(url){
+        return $http.get(url);
+    }
+    }
+}]);
+
+app.controller('appCTRL',['$scope', 'appFactory', function appCtrl($scope, appFactory){
+    
+    appFactory.recupData('competition.json').success(function(data){
+
+    $scope.tree = data;
+
+  });
+    
+}])
